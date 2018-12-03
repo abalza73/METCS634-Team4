3db2eb616cd58e1080aa40091a7c55e67d8a31ed
Antonio Balza <abalza73@gmail.com>
2018-12-02 22:30:37 -0500

    updating JS file to retrieve values from firebase

--- public/main.js
+++ public/main.js
@@ -1,11 +1,58 @@
 $(document).ready(function() {
     console.log("Ready!");
+
+    // Initialize Cloud Firestore
+    firebase.initializeApp({
+        apiKey: 'AIzaSyCJUiRfxTPoIuig0jxjvoaRPTcNgjgp5zc',
+        authDomain: 'carlos-libedinsky.firebaseapp.com',
+        projectId: 'carlos-libedinsky'
+    });
+  
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+  
+    // Disable deprecated features
+    db.settings({
+        timestampsInSnapshots: true
+    });
+    
+    // TODO: fix await functions
+    db.collection("concerts").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+        data2 += doc.data;
+        // doc.data() is never undefined for query doc snapshots
+        //console.log(doc.id, " => ", JSON.stringify(doc.data()));
+        var html = Mustache.render(template, data2);
+        });
+        console.log("data2: " + data2);
+    });
+
+    // Function to get concert information out of firebase
+    function getConcerts() {
+        var concerts;
+        db.collection("concerts").get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                console.log(doc.id, " => ", JSON.stringify(doc.data()));
+                concerts += JSON.stringify(doc.data);
+            });
+            console.log("concerts: " + concerts);
+        });
+      }
+      
+    async function main() {
+        var concertList = await getConcerts();
+        console.log("concert List: " + concertList);
+    }
+
+    main();
+
+    
     
     // This is the template for concerts html
     var template = document.getElementById("template").innerHTML;
     
     // This is the target div for concerts html 
-    var concerts = document.getElementById("concerts");
+    var concerts = document.getElementById("events");
 
     // Concert data    
     var data = {
